adbbce7b319c0c3ae19d92068a1777b05e2fd431
vijay-raghavendra <vijayamrutha7@gmail.com>
2025-11-02 19:43:23 +0530

    Spring Boot Aspect oriented Programming
    After returning advice

--- 10-spring-boot-Aspected-oriented-programming/16-spring-boot-aop-demo-after-returning-advice/src/main/java/com/aim2code/aopDemo/Aspect/MyDemoLoginAspect.java
+++ 10-spring-boot-Aspected-oriented-programming/16-spring-boot-aop-demo-after-returning-advice/src/main/java/com/aim2code/aopDemo/Aspect/MyDemoLoginAspect.java
@@ -1,6 +1,8 @@
 package com.aim2code.aopDemo.Aspect;
 
+import com.aim2code.aopDemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -8,12 +10,29 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(1)
 public class MyDemoLoginAspect {
 
     //this is where we add all of our related advices for logging
+
+    //add a new advice for @AfterReturning on the findAccounts()
+
+    @AfterReturning(
+            pointcut = "execution(* com.aim2code.aopDemo.DAO.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterreturningFindAccounts(JoinPoint joinPoint, List<Account> result) {
+        //print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n====> Execution @AfterReturning on method: "+method);
+
+        //print out the result of the method call
+        System.out.println("\n====>result is: "+result);
+    }
 
     //lets start with an @Before advice
     @Before("com.aim2code.aopDemo.Aspect.AimAOPPointCutExpressions.forDAOPackageNoSetterGetter()")
---
+++ 10-spring-boot-Aspected-oriented-programming/16-spring-boot-aop-demo-after-returning-advice/src/main/java/com/aim2code/aopDemo/DAO/AccountDAO.java
@@ -0,0 +1,24 @@
+package com.aim2code.aopDemo.DAO;
+
+import com.aim2code.aopDemo.Account;
+
+import java.util.List;
+
+public interface AccountDAO {
+
+    public void AddAccount(Account account, Boolean vipFlag);
+
+    Boolean checkAccount();
+
+    public String getServiceCode() ;
+
+    public void setServiceCode(String serviceCode) ;
+
+    public String getName() ;
+
+    public void setName(String name) ;
+
+    //Add a new method : findAccounts()
+
+    List<Account> findAccounts() ;
+}
