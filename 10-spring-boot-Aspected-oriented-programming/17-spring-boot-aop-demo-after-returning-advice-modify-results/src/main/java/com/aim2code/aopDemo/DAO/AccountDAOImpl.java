e56fd14d11f7fb814ed612c91b4cd75d07d67830
vijay-raghavendra <vijayamrutha7@gmail.com>
2025-11-10 15:51:13 +0530

    Spring Boot Aspect oriented Programming

---
+++ 10-spring-boot-Aspected-oriented-programming/17-spring-boot-aop-demo-after-returning-advice-modify-results/src/main/java/com/aim2code/aopDemo/DAO/AccountDAOImpl.java
@@ -0,0 +1,64 @@
+package com.aim2code.aopDemo.DAO;
+
+import com.aim2code.aopDemo.Account;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+
+    private String name;
+
+    private String serviceCode;
+
+    @Override
+    public void AddAccount(Account account,Boolean vipFlag) {
+        System.out.println(getClass()+": Doing my DB work: ADDING AN ACCOUNT");
+    }
+
+    @Override
+    public Boolean checkAccount() {
+        System.out.println(getClass()+": inside checkAccount()");
+        return true;
+    }
+
+    public String getServiceCode() {
+        System.out.println(getClass()+": inside getServiceCode()");
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        System.out.println(getClass()+": inside setServiceCode()");
+        this.serviceCode = serviceCode;
+    }
+
+    public String getName() {
+        System.out.println(getClass()+": inside getName()");
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println(getClass()+": inside setName()");
+        this.name = name;
+    }
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> accounts = new ArrayList<>();
+
+        //create sample accounts
+        Account account1 = new Account("vijay","004110");
+        Account account2 = new Account("Naveen","004111");
+        Account account3 = new Account("Sushanth","004112");
+        //add them to our account list
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+
+        //System.out.println(getClass()+": inside findAccounts() from AccountsDAOImpl");
+
+        return accounts;
+    }
+}
